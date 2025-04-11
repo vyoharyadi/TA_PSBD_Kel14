d74f0cc6e682266d7d2d0b601846ed5e02a44850
Vyo Haryadi <vyoharyadi@gmail.com>
2025-04-11 16:09:30 +0700

    Rapihin

--- database/seeders/BookCategorySeeder.php
+++ database/seeders/BookCategorySeeder.php
@@ -17,7 +17,6 @@ class BookCategorySeeder extends Seeder
         $categoryIds = Category::pluck('id');
 
         foreach ($books as $book) {
-            // Ambil 1-3 kategori secara acak untuk setiap buku
             $randomCategoryIds = $categoryIds->random(rand(1, 3));
             $book->categories()->attach($randomCategoryIds);
         }
